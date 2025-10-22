eef3d4137fcee11d8f8e28656b0dc22dd81805a5
tofu2023 <2214994328@qq.com>
2025-10-22 11:43:36 +0900

    feat:动物图鉴

--- views/animal_album/animal_album.gd
+++ views/animal_album/animal_album.gd
@@ -13,3 +13,10 @@ func _process(delta: float) -> void:
 		$CarouselContainer._left()
 	if Input.is_action_just_pressed("right"):
 		$CarouselContainer._right()
+	if Input.is_action_just_pressed("E"):
+		$ColorRect.visible = true
+		$ColorRect/Timer.start()
+
+
+func _on_timer_timeout() -> void:
+	$ColorRect.visible = false
